33f7e392c64154dc7fb57abc9ced74ce6f603dda
BBuf <1182563586@qq.com>
2021-04-02 13:50:30 +0800

    add oneflow_onnx source

--- README.md
+++ README.md
@@ -1,6 +1,50 @@
-# oneflow_convert_tools
+## oneflow_convert_tools
 
-OneFlow相关的模型转换工具汇总。
+OneFlow相关的模型转换工具
 
-## nchw2nhwc_tool
-- OneFlow训练的NCHW排布的权重转换为NHWC排布
+### onnx
+
+#### 简介
+
+onnx工具包含两个功能，一个是将OneFlow导出ONNX，另外一个是将各个训练框架导出的ONNX模型转换为OneFlow的模型。本工程已经适配了TensorFlow/Pytorch/Paddle框架的预训练模型通过导出ONNX转换为OneFlow（我们将这一功能叫作X2OneFlow）。
+
+#### 环境依赖
+
+##### 用户环境配置
+
+```sh
+python>=3.5
+onnx>=1.8.0
+onnx-simplifier>=0.3.3
+onnxoptimizer>=0.2.5
+onnxruntime>=1.6.0
+oneflow>=0.3.4
+```
+
+#### 安装
+
+##### 安装方式1
+
+```sh
+pip install oneflow_onnx
+```
+
+#### 安装方式2
+
+```
+git clone https://github.com/Oneflow-Inc/oneflow_convert_tools
+cd oneflow_onnx
+python3 setup.py install
+```
+
+```sh
+git clone https://github.com/Oneflow-Inc/oneflow_convert_tools
+python setup.py install
+```
+
+### nchw2nhwc_tool
+
+#### 简介
+
+本工具的功能是将OneFlow训练的NCHW排布的权重转换为NHWC排布，使用方法[在这里](nchw2nhwc_tool/README.md)
+
---
+++ setup.py
@@ -0,0 +1,42 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from __future__ import absolute_import
+import setuptools
+
+long_description = "oneflow_onnx is a toolkit for converting trained model of OneFlow to ONNX and ONNX to OneFlow.\n\n"
+long_description += "Usage: oneflow_onnx --model_dir src --save_file dist\n"
+long_description += "GitHub: https://github.com/Oneflow-Inc/oneflow_convert_tools/oneflow_onnx\n"
+long_description += "Email: zhangxiaoyu@oneflow.org"
+
+setuptools.setup(
+    name="oneflow_onnx",
+    version=0.6,
+    author="zhangxiaoyu",
+    author_email="zhangxiaoyu@oneflow.org",
+    description="a toolkit for converting trained model of OneFlow to ONNX and ONNX to OneFlow.",
+    long_description=long_description,
+    long_description_content_type="text/plain",
+    url="https://github.com/Oneflow-Inc/oneflow_convert_tools/oneflow_onnx",
+    packages=setuptools.find_packages(),
+    install_requires=['six', 'protobuf'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    license='Apache 2.0',
+    entry_points={'console_scripts': ['oneflow_onnx=oneflow_onnx.command:main']})
