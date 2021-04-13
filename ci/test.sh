455d091380ad638ea634b85efcf926184c6cfed7
tsai <jackalcooper@gmail.com>
2021-04-13 19:07:20 +0800

    refine

--- ci/test.sh
+++ ci/test.sh
@@ -2,7 +2,7 @@ set -ex
 python3 -m pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 python3 -m pip install --upgrade pip
 python3 -m pip install flake8 pytest
-if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
+if [ -f requirements.txt ]; then pip install --user -r requirements.txt; fi
 python3 -m pip install oneflow --user -f https://staging.oneflow.info/branch/master/cu102
 python3 setup.py install
 python3 -m pytest examples/oneflow2onnx
