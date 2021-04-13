7af6e79881917176064e63ad06175dda628aadfd
tsai <jackalcooper@gmail.com>
2021-04-13 19:08:21 +0800

    refine

--- ci/test.sh
+++ ci/test.sh
@@ -1,10 +1,10 @@
 set -ex
 python3 -m pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
-python3 -m pip install --upgrade pip
-python3 -m pip install flake8 pytest
+python3 -m pip install --user --upgrade pip
+python3 -m pip install --user flake8 pytest
 if [ -f requirements.txt ]; then pip install --user -r requirements.txt; fi
 python3 -m pip install oneflow --user -f https://staging.oneflow.info/branch/master/cu102
-python3 setup.py install
+python3 setup.py install --user
 python3 -m pytest examples/oneflow2onnx
 python3 -m pytest examples/x2oneflow/pytorch2oneflow/nodes
 python3 -m pytest examples/x2oneflow/tensorflow2oneflow/nodes
