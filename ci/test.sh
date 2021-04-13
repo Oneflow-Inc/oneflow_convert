f9ffc564e39c53228171a171abdaf2778e6ec058
tsai <jackalcooper@gmail.com>
2021-04-13 19:08:28 +0800

    refine

--- ci/test.sh
+++ ci/test.sh
@@ -4,7 +4,7 @@ python3 -m pip install --user --upgrade pip
 python3 -m pip install --user flake8 pytest
 if [ -f requirements.txt ]; then pip install --user -r requirements.txt; fi
 python3 -m pip install oneflow --user -f https://staging.oneflow.info/branch/master/cu102
-python3 setup.py install --user
+python3 setup.py install
 python3 -m pytest examples/oneflow2onnx
 python3 -m pytest examples/x2oneflow/pytorch2oneflow/nodes
 python3 -m pytest examples/x2oneflow/tensorflow2oneflow/nodes
