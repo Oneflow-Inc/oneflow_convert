37cf7d5bd5de9fdd24880ac67a86aca25ff4eec6
BBuf <1182563586@qq.com>
2021-04-15 09:37:16 +0800

    add oneflow_onnx source

--- ci/test.sh
+++ ci/test.sh
@@ -6,7 +6,9 @@ if [ -f requirements.txt ]; then pip install --user -r requirements.txt; fi
 python3 -m pip install oneflow --user -U -f https://staging.oneflow.info/branch/master/cu102
 python3 setup.py install
 python3 -m pytest examples/oneflow2onnx
-python3 -m pytest examples/x2oneflow/pytorch2oneflow
-python3 -m pytest examples/x2oneflow/tensorflow2oneflow
+python3 -m pytest examples/x2oneflow/pytorch2oneflow/nodes
+python3 -m pytest examples/x2oneflow/pytorch2oneflow/models
+python3 -m pytest examples/x2oneflow/tensorflow2oneflow/nodes
+python3 -m pytest examples/x2oneflow/tensorflow2oneflow/models
 python3 -m pytest examples/x2oneflow/paddle2oneflow/nodes
 sh test.sh
