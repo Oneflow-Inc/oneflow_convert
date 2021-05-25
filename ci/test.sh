bc223f43f2915a0d5ee2da06b22bcce354acebaf
mosout <mosout@qq.com>
2021-05-25 10:49:24 +0800

    update ci

--- ci/test.sh
+++ ci/test.sh
@@ -2,6 +2,7 @@ set -ex
 python3 -m pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 python3 -m pip install --user --upgrade pip
 python3 -m pip install --user flake8 pytest
+python3 -m pip install nvidia-pyindex
 if [ -f requirements.txt ]; then pip install --user -r requirements.txt; fi
 python3 -m pip install oneflow --user -U -f https://staging.oneflow.info/branch/master/cu102
 python3 setup.py install
