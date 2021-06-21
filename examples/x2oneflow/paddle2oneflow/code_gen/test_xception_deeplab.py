d8d16a6fd47e0841c416a7e98151766b7fc5d907
BBuf <1182563586@qq.com>
2021-06-21 15:05:50 +0800

    uncomment cleanup onnx model

--- examples/x2oneflow/paddle2oneflow/code_gen/test_xception_deeplab.py
+++ examples/x2oneflow/paddle2oneflow/code_gen/test_xception_deeplab.py
@@ -446,3 +446,5 @@ def test_Xception41_deeplab():
     load_paddle_module_and_check(
         Xception41_deeplab, input_size=(1, 3, 224, 224), train_flag=False, flow_weight_dir="/tmp/oneflow", oneflow_code_gen_flag=True
     )
+
+test_Xception41_deeplab()
