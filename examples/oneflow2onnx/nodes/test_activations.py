10f1afd4b9e5c9617df2d093ac42f84448f3d8b5
lixiang007666 <88304454@qq.com>
2021-12-10 15:55:36 +0800

    Add prelu single test

--- examples/oneflow2onnx/nodes/test_activations.py
+++ examples/oneflow2onnx/nodes/test_activations.py
@@ -14,6 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import tempfile
+import random
 import oneflow as flow
 from oneflow_onnx.oneflow2onnx.util import convert_to_onnx_and_check
 
@@ -29,6 +30,18 @@ class ReLUOpGraph(flow.nn.Graph):
         return out
 
 
+prelu = flow.nn.PReLU()
+prelu = prelu.to("cuda")
+class PReLUOpGraph(flow.nn.Graph):
+    def __init__(self):
+        super().__init__()
+        self.m = prelu
+
+    def build(self, x):
+        out = self.m(x)
+        return out
+
+
 def test_relu():
     
     relu_graph = ReLUOpGraph()
@@ -38,4 +51,28 @@ def test_relu():
         flow.save(relu.state_dict(), tmpdirname)
         convert_to_onnx_and_check(relu_graph, flow_weight_dir=tmpdirname, onnx_model_path="/tmp")
 
+def test_prelu_one_channels():
+    
+    prelu_graph = PReLUOpGraph()
+    prelu_graph._compile(flow.randn(1, 1, 224, 224).to("cuda"))
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        flow.save(prelu.state_dict(), tmpdirname)
+        convert_to_onnx_and_check(prelu_graph, flow_weight_dir=tmpdirname, onnx_model_path="/tmp")
+
+def test_prelu_n_channels():
+    
+    prelu_graph = PReLUOpGraph()
+    channels=random.randint(2,10)
+    prelu_graph._compile(flow.randn(1, channels, 224, 224).to("cuda"))
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        flow.save(prelu.state_dict(), tmpdirname)
+        convert_to_onnx_and_check(prelu_graph, flow_weight_dir=tmpdirname, onnx_model_path="/tmp")
+
+
+test_prelu_one_channels()
+test_prelu_n_channels()
 test_relu()
+
+
