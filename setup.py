7efcd59733ceba9b8f1f06a622157e47bf8b9fb9
bbuf <1182563586@qq.com>
2022-01-05 16:39:36 +0800

    release 0.5.4

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ long_description += "Email: zhangxiaoyu@oneflow.org"
 
 setuptools.setup(
     name="oneflow_onnx",
-    version="0.5.3",
+    version="0.5.4",
     author="zhangxiaoyu",
     author_email="zhangxiaoyu@oneflow.org",
     description="a toolkit for converting trained model of OneFlow to ONNX.",
