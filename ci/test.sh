4fd7adcc69b1343f84fb14c5e178c392dd76322f
Xiaoyu Zhang <35585791+BBuf@users.noreply.github.com>
2022-05-22 11:02:18 +0800

    Update test.sh

--- ci/test.sh
+++ ci/test.sh
@@ -2,7 +2,7 @@ set -ex
 python3 -m pip config set global.index-url https://mirrors.bfsu.edu.cn/pypi/web/simple
 python3 -m pip install --user --upgrade pip
 if [ -f requirements.txt ]; then python3 -m pip install -r requirements.txt --user; fi
-python3 -m pip install --pre oneflow -f https://staging.oneflow.info/branch/release/debug-CMAKE_INTERPROCEDURAL_OPTIMIZATION-OFF/cu112
+python3 -m pip install --pre oneflow -f https://staging.oneflow.info/branch/master/cu112
 python3 setup.py install
 pip install flowvision==0.0.3 --user
 python3 -m pytest examples/oneflow2onnx/models/CPU
