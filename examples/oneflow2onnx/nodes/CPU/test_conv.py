58ca62e9fba976095fc61928e497134c36782817
BBuf <1182563586@qq.com>
2022-09-08 10:24:32 +0800

    refine

--- examples/oneflow2onnx/nodes/CPU/test_conv.py
+++ examples/oneflow2onnx/nodes/CPU/test_conv.py
@@ -43,6 +43,15 @@ def test_conv2d():
 
     convert_to_onnx_and_check(conv_graph, onnx_model_path="/tmp")
 
+def test_conv2d_flow_weight_dir():
+    
+    conv_graph = Conv2dOpGraph()
+    conv_graph._compile(flow.randn(1, 3, 224, 224))
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        flow.save(conv_module.state_dict(), tmpdirname)
+        convert_to_onnx_and_check(conv_graph, flow_weight_dir=tmpdirname, onnx_model_path="/tmp")
+
 def test_conv2d_opset14():
     
     conv_graph = Conv2dOpGraph()
@@ -52,4 +61,6 @@ def test_conv2d_opset14():
 
 
 test_conv2d()
+test_conv2d_flow_weight_dir()
 test_conv2d_opset14()
+
