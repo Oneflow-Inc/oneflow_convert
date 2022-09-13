5d1b004d27861b30da5e025d682a28edc29fab0e
Xiaoyu Zhang <35585791+BBuf@users.noreply.github.com>
2022-09-13 20:49:23 +0800

    support free eager tensor (#89)

---
+++ examples/oneflow2onnx/models/CPU/test_free_eager_tensor.py
@@ -0,0 +1,31 @@
+import oneflow as flow
+import oneflow.nn as nn
+from oneflow_onnx.oneflow2onnx.util import convert_to_onnx_and_check
+
+
+class ConvModule(nn.Module):
+    def __init__(self):
+        super(ConvModule, self).__init__()
+        self.zero = flow.zeros(1, 2, 3, 3)
+        self.conv = nn.Conv2d(1, 2, 1, 1)
+
+    def forward(self, x):
+        return self.conv(x) + self.zero
+
+
+m = ConvModule()
+
+
+class YOLOGraph(flow.nn.Graph):
+    def __init__(self, m):
+        super().__init__()
+        self.model = m
+
+    def build(self, x):
+        return self.model(x)
+
+
+yolo_graph = YOLOGraph(m)
+yolo_graph._compile(flow.randn(1, 1, 3, 3))
+
+convert_to_onnx_and_check(yolo_graph, onnx_model_path="/tmp", print_outlier=True)
