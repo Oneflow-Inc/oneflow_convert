98d2eb6618abaf5275e69311727f7b5404916334
BBuf <1182563586@qq.com>
2022-09-13 02:01:36 +0000

    fix ci error

--- ci/test_black_fmt.sh
+++ ci/test_black_fmt.sh
@@ -1,6 +1,7 @@
 set -ex
 python3 -m pip config set global.index-url https://mirrors.bfsu.edu.cn/pypi/web/simple
 python3 -m pip install --user --upgrade pip
+python3 -m pip install click==8.0.4
 python -m pip install black==21.4b2
 
 black -l 200 --check oneflow_onnx/ examples/
