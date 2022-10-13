cbb59e41d647e90ce3bfded6219d4b2c3cc8e424
bbuf <1182563586@qq.com>
2022-10-13 11:30:38 +0800

    refine

--- oneflow_onnx/oneflow2onnx/util.py
+++ oneflow_onnx/oneflow2onnx/util.py
@@ -149,11 +149,20 @@ def convert_to_onnx_and_check(
         if oneflow_res is not None:
             if not isinstance(oneflow_res, np.ndarray):
                 if flow.is_tensor(oneflow_res):
-                    if "global" in device:
-                        oneflow_res = oneflow_res.to_local()
-                    oneflow_res = oneflow_res.numpy()
+                    pass
+                elif isinstance(oneflow_res, dict):
+                    for key, value in oneflow_res.items():
+                        oneflow_res = value
+                        break
+                elif isinstance(oneflow_res, list):
+                    oneflow_res = oneflow_res[0]
                 else:
-                    oneflow_res = oneflow_res[0].numpy()
+                    raise NotImplementedError
+            if flow.is_tensor(oneflow_res):
+                if "global" in device:
+                    oneflow_res = oneflow_res.to_local()
+                oneflow_res = oneflow_res.numpy()
+
             compare_result(oneflow_res, onnx_res, print_outlier=print_outlier)
 
     # cleanup()
