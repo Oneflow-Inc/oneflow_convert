708323a947f30cee617184c49cf9fb3c1cbe5736
qmpzzpmq <405691733@qq.com>
2023-02-17 12:41:51 +0800

    add tset

---
+++ examples/oneflow2onnx/nodes/CPU/test_stack.py
@@ -0,0 +1,50 @@
+"""
+Copyright 2020 The OneFlow Authors. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+import tempfile
+import oneflow as flow
+from oneflow_onnx.oneflow2onnx.util import convert_to_onnx_and_check
+
+
+class Stack(flow.nn.Module):
+    def __init__(self) -> None:
+        super(Stack, self).__init__()
+
+    def forward(self, x: flow.Tensor) -> flow.Tensor:
+        return flow.stack([x, x, x], dim=1)
+
+
+stack = Stack()
+
+
+class StackOpGraph(flow.nn.Graph):
+    def __init__(self):
+        super().__init__()
+        self.m = stack
+
+    def build(self, x):
+        out = self.m(x)
+        return out
+
+
+def test_stack():
+
+    stack_graph = StackOpGraph()
+    stack_graph._compile(flow.randn(1, 3, 224, 224))
+
+    convert_to_onnx_and_check(stack_graph, onnx_model_path="./temp")
+
+
+test_stack()
